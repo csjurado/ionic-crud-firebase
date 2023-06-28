26b87086ba595b6cc2217e4f3039aed22c580348
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2023-06-28 14:53:27 -0500

    Proyecto funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
 const routes: Routes = [
   {
     path: 'home',
@@ -11,8 +10,15 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'make-appointment',
+    loadChildren: () => import('./make-appointment/make-appointment.module').then( m => m.MakeAppointmentPageModule)
+  },
+  {
+    path: 'edit-appointment/:id',
+    loadChildren: () => import('./edit-appointment/edit-appointment.module').then( m => m.EditAppointmentPageModule)
+  },
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCBnLP64i3Tsqg12F8sTmqr1nnsQaPo7dM",
+    authDomain: "app2023a-af122.firebaseapp.com",
+    projectId: "app2023a-af122",
+    storageBucket: "app2023a-af122.appspot.com",
+    messagingSenderId: "363003734298",
+    appId: "1:363003734298:web:d41f7193d7aaecd22d64a3",
+    measurementId: "G-S19XY4Y1F6"
+  }
+}
